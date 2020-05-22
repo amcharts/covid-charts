e5e21ab476a4b9b7cbdcdb9e341f93d522041ec6
martynasma <martynasma@gmail.com>
2020-05-22 22:00:24 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":5178911,"deaths":336404,"recovered":1992440,"date":"2020-05-22"}
+var covid_total_current = {"confirmed":5180982,"deaths":336432,"recovered":1992496,"date":"2020-05-22"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":1593195,"deaths":95681,"recovered":298219,"date":"2020-05-22"}
+var covid_us_total_current = {"confirmed":1595193,"deaths":95705,"recovered":298219,"date":"2020-05-22"}
