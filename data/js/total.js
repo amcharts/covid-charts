cd1af88031d2129443cdd0c3c49b292fa8466f20
martynasma <martynasma@gmail.com>
2020-06-23 18:00:30 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":9150391,"deaths":473519,"recovered":4579851,"date":"2020-06-23"}
+var covid_total_current = {"confirmed":9154232,"deaths":473650,"recovered":4585022,"date":"2020-06-23"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":2316587,"deaths":120509,"recovered":639942,"date":"2020-06-23"}
+var covid_us_total_current = {"confirmed":2318802,"deaths":120606,"recovered":639942,"date":"2020-06-23"}
