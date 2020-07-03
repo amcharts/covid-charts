db7758f7e3614431d652198ba61237e614ef848d
martynasma <martynasma@gmail.com>
2020-07-03 03:00:35 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":10836500,"deaths":520634,"recovered":5723808,"date":"2020-07-03"}
+var covid_total_current = {"confirmed":10842416,"deaths":520781,"recovered":5724742,"date":"2020-07-03"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":2727391,"deaths":128515,"recovered":781697,"date":"2020-07-03"}
+var covid_us_total_current = {"confirmed":2730929,"deaths":128573,"recovered":781697,"date":"2020-07-03"}
