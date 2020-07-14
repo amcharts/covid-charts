cfa3da6aface1539a0943531f84cc10c70200f63
martynasma <martynasma@gmail.com>
2020-07-14 00:00:32 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":12720028,"deaths":565178,"recovered":7005452,"date":"2020-07-13"}
+var covid_total_current = {"confirmed":12720028,"deaths":565178,"recovered":7005452,"date":"2020-07-14"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":3237656,"deaths":134631,"recovered":995263,"date":"2020-07-13"}
+var covid_us_total_current = {"confirmed":3237656,"deaths":134631,"recovered":995263,"date":"2020-07-14"}
