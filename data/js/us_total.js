af4cc5f9849ef2918fe0a798b780edf083b4a2a8
martynasma <martynasma@gmail.com>
2020-09-03 14:01:00 +0000

    Data updated

--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":6078073,"deaths":185271,"recovered":2230077,"date":"2020-09-03"}
+var covid_us_total_current = {"confirmed":6078527,"deaths":185275,"recovered":2230077,"date":"2020-09-03"}
