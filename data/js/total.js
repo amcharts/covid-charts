b1cf8c503b00faf8d0afe4a88f12c650970aa45e
martynasma <martynasma@gmail.com>
2020-10-17 18:01:09 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":39461816,"deaths":1106454,"recovered":27144753,"date":"2020-10-17"}
+var covid_total_current = {"confirmed":39485409,"deaths":1106519,"recovered":27145486,"date":"2020-10-17"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":8008101,"deaths":217962,"recovered":3168621,"date":"2020-10-17"}
+var covid_us_total_current = {"confirmed":8014348,"deaths":218019,"recovered":3168621,"date":"2020-10-17"}
