33be789ab74c307a8bb9ba695a160c1753c2d125
martynasma <martynasma@gmail.com>
2020-10-24 16:01:11 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":42307731,"deaths":1145847,"recovered":28615456,"date":"2020-10-24"}
+var covid_total_current = {"confirmed":42343866,"deaths":1146221,"recovered":28621007,"date":"2020-10-24"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":8432125,"deaths":223169,"recovered":3345160,"date":"2020-10-24"}
+var covid_us_total_current = {"confirmed":8441460,"deaths":223299,"recovered":3345160,"date":"2020-10-24"}
