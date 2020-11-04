38f2c2b1e1ca523418158d4673196f299d6e7e42
martynasma <martynasma@gmail.com>
2020-11-04 03:01:20 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":47328401,"deaths":1212070,"recovered":31523055,"date":"2020-11-04"}
+var covid_total_current = {"confirmed":47348201,"deaths":1212844,"recovered":31553108,"date":"2020-11-04"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":9302191,"deaths":231589,"recovered":3670790,"date":"2020-11-04"}
+var covid_us_total_current = {"confirmed":9304202,"deaths":231604,"recovered":3670790,"date":"2020-11-04"}
