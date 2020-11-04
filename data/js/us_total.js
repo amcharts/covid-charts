ecc83876cd371298600f16ccbc72334b5571e6a0
martynasma <martynasma@gmail.com>
2020-11-04 17:01:18 +0000

    Data updated

--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":9315167,"deaths":231732,"recovered":3670790,"date":"2020-11-04"}
+var covid_us_total_current = {"confirmed":9318723,"deaths":231781,"recovered":3670790,"date":"2020-11-04"}
