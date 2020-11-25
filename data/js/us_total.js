71f2fdc58b39c5ad7ad326cf93986c2a2c09a7b3
martynasma <martynasma@gmail.com>
2020-11-25 05:01:29 +0000

    Data updated

--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":12532452,"deaths":258711,"recovered":4650361,"date":"2020-11-25"}
+var covid_us_total_current = {"confirmed":12534448,"deaths":258712,"recovered":4650361,"date":"2020-11-25"}
