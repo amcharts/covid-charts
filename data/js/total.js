5eeb95419c0655c374b07bb3373891c009510ea6
martynasma <martynasma@gmail.com>
2020-12-12 14:01:38 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":71211472,"deaths":1597629,"recovered":45479495,"date":"2020-12-12"}
+var covid_total_current = {"confirmed":71226341,"deaths":1597843,"recovered":45484056,"date":"2020-12-12"}
