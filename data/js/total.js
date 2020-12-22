caca9497322789d699a39f88c03b80dac42673a5
martynasma <martynasma@gmail.com>
2020-12-22 00:01:31 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":77269379,"deaths":1700399,"recovered":43551322,"date":"2020-12-21"}
+var covid_total_current = {"confirmed":77296617,"deaths":1700932,"recovered":43566356,"date":"2020-12-22"}
