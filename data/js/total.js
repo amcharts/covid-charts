d02b79839e5b29827aa7db46081edc01e9f58425
martynasma <martynasma@gmail.com>
2021-01-07 20:02:00 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":87588168,"deaths":1890824,"recovered":48944445,"date":"2021-01-07"}
+var covid_total_current = {"confirmed":87654861,"deaths":1891260,"recovered":48945110,"date":"2021-01-07"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":21304659,"deaths":361087,"recovered":8398881,"date":"2021-01-07"}
+var covid_us_total_current = {"confirmed":21326349,"deaths":361242,"recovered":8398881,"date":"2021-01-07"}
