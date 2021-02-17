7085175bc5660c7cd50d17ae092505b4251ad0b4
martynasma <martynasma@gmail.com>
2021-02-17 00:01:45 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":109449313,"deaths":2416282,"recovered":61528693,"date":"2021-02-16"}
+var covid_total_current = {"confirmed":109463210,"deaths":2416661,"recovered":61541518,"date":"2021-02-17"}
--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":27635243,"deaths":485572,"recovered":11626193,"date":"2021-02-16"}
+var covid_us_total_current = {"confirmed":27638030,"deaths":485626,"recovered":11626193,"date":"2021-02-17"}
