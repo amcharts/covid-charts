b55356eb70b3d530ee567cd97046dc286c518046
martynasma <martynasma@gmail.com>
2021-04-20 21:01:41 +0000

    Data updated

--- data/js/total.js
+++ data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":142391951,"deaths":3035163,"recovered":81570374,"date":"2021-04-20"}
+var covid_total_current = {"confirmed":142453783,"deaths":3035771,"recovered":81603637,"date":"2021-04-20"}
