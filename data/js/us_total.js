f0cde66385e30ea47bbe0f3ff7f59a8efea2c2a2
martynasma <martynasma@gmail.com>
2021-07-07 02:01:49 +0000

    Data updated

--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":33593351,"deaths":603176,"recovered":0,"date":"2021-07-07"}
+var covid_us_total_current = {"confirmed":33593350,"deaths":603176,"recovered":0,"date":"2021-07-07"}
