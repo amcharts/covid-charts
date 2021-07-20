bed70a5a931441b8e37100c1d9275bd0a278c855
martynasma <martynasma@gmail.com>
2021-07-20 01:01:49 +0000

    Data updated

--- data/js/us_total.js
+++ data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":33974448,"deaths":606486,"recovered":0,"date":"2021-07-20"}
+var covid_us_total_current = {"confirmed":33974450,"deaths":606486,"recovered":0,"date":"2021-07-20"}
